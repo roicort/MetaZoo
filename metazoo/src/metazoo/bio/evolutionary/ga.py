837734c000b89c74b3dd1ab873034f355e2619c3
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-09-12 14:43:44 -0600

    Refactor GA to use encoding classes
    Replaces string-based encoding logic in GeneticAlgorithm with an Encoding class hierarchy (Binary, Real, Permutation). Population initialization and decoding are now handled by encoding objects, improving extensibility and separation of concerns.

--- metazoo/src/metazoo/bio/evolutionary/ga.py
+++ metazoo/src/metazoo/bio/evolutionary/ga.py
@@ -7,7 +7,7 @@ from rich.table import Table
 from rich.console import Console
 from plotly import express as px
 
-from .utils import Population
+from .utils import Population, Encoding
 
 
 class GeneticAlgorithm:
@@ -20,64 +20,23 @@ class GeneticAlgorithm:
         ],
         mutation_function: Callable[[np.ndarray], np.ndarray],
         selection_function: Callable[[np.ndarray], np.ndarray],
-        encoding: str,
-        population_size: int,
+        encoder: Encoding,
         mutation_rate: float = 0.01,
         crossover_rate: float = 0.7,
-        precision: int = 3,  # Number of bits per variable for binary encoding
-        bounds: Optional[Sequence[Tuple[float, float]]] = None,
         minimize: bool = True,
         elitism: float = None,  # Percentage of best individuals to preserve
+        population_size: int = 100,
     ):
-        self.population_size = population_size
         self.fitness_function = fitness_function
         self.mutation_function = mutation_function
         self.crossover_function = crossover_function
         self.selection_function = selection_function
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
-        self.encoding = encoding
         self.minimize = minimize
-        self.genome_length = None
         self.elitism = elitism
 
-        self.epsilon = 10 ** (
-            -precision
-        )  # ε = Obtaianable accuracy for binary encoding
-
-        if bounds is None:
-            raise ValueError(
-                "Bounds must be provided as (low, high) or a sequence of (low, high)."
-            )
-        else:
-            self.bounds = bounds
-        self.dim = np.sum([1 for _ in bounds])  # Dimension inferred from bounds
-
-        # Bits per variable for binary encoding
-        # This is calculated based on the precision and the range of each variable
-        # Using the formula: n = ceil(log2((XU - XL) / ε))
-        # where XU and XL are the upper and lower bounds of the variable, and ε is the desired precision
-        # We can understand ε as the smallest difference we want to be able to represent between two values of the variable
-        self.bits_per_var = max(
-            int(np.ceil(np.log2((xu - xl) / self.epsilon))) for xl, xu in self.bounds
-        )
-        # We use max to ensure we have enough bits for the most constrained variable
-
-        # Get best genome_length
-        if self.genome_length is None:
-            if self.encoding == "real":
-                self.genome_length = self.dim
-            elif self.encoding == "binary":
-                # For binary encoding, genome length is bits per variable times number of variables
-                self.genome_length = self.bits_per_var * self.dim
-
-        # Initialize population
-        self.population = Population(
-            population_size=self.population_size,
-            genome_length=self.genome_length,
-            bounds=self.bounds,
-            encoding=self.encoding,
-        ).initialize()
+        self.population = Population(population_size, encoder)
 
         self.best_individual = None
         self.best_fitness = -np.inf
@@ -95,7 +54,7 @@ class GeneticAlgorithm:
         table.add_row("Genome Length", str(self.genome_length))
         table.add_row("Mutation Rate", str(self.mutation_rate))
         table.add_row("Crossover Rate", str(self.crossover_rate))
-        table.add_row("Encoding", str(self.encoding))
+        #table.add_row("Encoding", str(self.encoding))
         table.add_row("Selection Function", self.selection_function.__name__)
         table.add_row("Crossover Function", self.crossover_function.__name__)
         table.add_row("Mutation Function", self.mutation_function.__name__)
@@ -104,11 +63,11 @@ class GeneticAlgorithm:
         table.add_row(
             "Elitism", str(self.elitism) if self.elitism is not None else "None"
         )
-        if self.encoding == "binary":
-            table.add_row("Epsilon", str(self.epsilon))
-            table.add_row("Bits Per Var", str(self.bits_per_var))
-            table.add_row("Genome Length", str(self.genome_length))
-        table.add_row("Bounds", str(self.bounds))
+        #if self.encoding == "binary":
+        #    table.add_row("Epsilon", str(self.epsilon))
+        #    table.add_row("Bits Per Var", str(self.bits_per_var))
+        #    table.add_row("Genome Length", str(self.genome_length))
+
         table.add_row("Minimize", str(self.minimize))
         console = Console()
         console.print(table)
@@ -119,17 +78,13 @@ class GeneticAlgorithm:
         """
         
         # Raw Fitness.
-        if self.encoding == "binary":
-            raw_fitness = np.array(
-                [
-                    self.fitness_function(self.decode(individual))
-                    for individual in self.population
-                ]
-            )
-        else:
-            raw_fitness = np.array(
-                [self.fitness_function(individual) for individual in self.population]
-            )
+
+        raw_fitness = np.array(
+            [
+                self.fitness_function(self.population.encoding.decode(individual))
+                for individual in self.population.individuals
+            ]
+        )
 
         if self.minimize:
             fitness = np.nan_to_num(raw_fitness, nan=1e10, posinf=1e10, neginf=1e10)
@@ -142,10 +97,8 @@ class GeneticAlgorithm:
             best_idx = int(fitness.argmax())
             fitness_transformed = fitness
 
-        bestcandidate = self.population[best_idx]
-        self.best_individual = (
-            bestcandidate if self.encoding == "real" else self.decode(bestcandidate)
-        )
+        bestcandidate = self.population.individuals[best_idx]
+        self.best_individual = self.population.encoding.decode(bestcandidate)
 
         return fitness, fitness_transformed
 
@@ -156,8 +109,8 @@ class GeneticAlgorithm:
         fitness, fitness_transformed = self.eval()
         self.fitness_history.append(fitness.mean())
         self.best_history.append(self.best_fitness)
-        selected_indices = self.selection_function(self.population, fitness_transformed)
-        selected_parents = self.population[selected_indices]
+        selected_indices = self.selection_function(self.population.individuals, fitness_transformed)
+        selected_parents = self.population.individuals[selected_indices]
         next_generation = self.create_descendants(selected_parents)
 
         if self.elitism is not None:
@@ -168,7 +121,7 @@ class GeneticAlgorithm:
             # This garantees that the best solution found so far is always preserved.
             # Think that in nature, the best individuals are more likely to survive and reproduce, passing their genes to the next generation.
             # Preserve N% of the best individuals
-            N = max(1, int(self.elitism * self.population_size))
+            N = max(1, int(self.elitism * self.population.size))
             if self.minimize:
                 elite_indices = np.argsort(fitness)[
                     :N
@@ -177,12 +130,12 @@ class GeneticAlgorithm:
                 elite_indices = np.argsort(fitness)[
                     -N:
                 ]  # Indices of the N best individuals (maximization)
-            elites = self.population[elite_indices]
+            elites = self.population.individuals[elite_indices]
             next_generation[:N] = (
                 elites  # Replace the first N individuals with the elites
             )
 
-        self.population = next_generation
+        self.population.individuals = next_generation
 
     def create_descendants(self, parents: np.ndarray) -> np.ndarray:
         """
@@ -192,7 +145,7 @@ class GeneticAlgorithm:
         if len(parents) < 2:
             raise ValueError("Not enough parents to create descendants.")
         next_generation = []
-        for _ in range(self.population_size // 2):
+        for _ in range(self.population.size // 2):
             # Select two parents
             idx1, idx2 = np.random.choice(len(parents), size=2, replace=False)
             parent1 = parents[idx1]
@@ -205,23 +158,6 @@ class GeneticAlgorithm:
 
         return np.array(next_generation)
 
-    def decode(self, individual: np.ndarray) -> np.ndarray:
-        """
-        Decodes a binary individual into its real-valued representation.
-        """
-        if self.encoding == "binary":
-            bits_per_var = self.bits_per_var
-            decoded = []
-            for i, (a, b) in enumerate(self.bounds):
-                bits = individual[i * bits_per_var : (i + 1) * bits_per_var]
-                value = int("".join(str(int(bit)) for bit in bits), 2)
-                max_value = 2**bits_per_var - 1
-                real_value = a + (b - a) * value / max_value
-                decoded.append(real_value)
-            return np.array(decoded)
-        else:
-            raise NotImplementedError("Decoding not implemented for this encoding.")
-
     def run(
         self, generations: int, history: bool = False, verbose: bool = True
     ) -> list[np.ndarray]:
@@ -231,15 +167,14 @@ class GeneticAlgorithm:
                 task = progress.add_task("Evolving...", total=generations)
                 for _ in range(generations):
                     self.evolve()
-                    pop_history.append(self.population.copy())
+                    pop_history.append(self.population.individuals.copy())
                     progress.advance(task)
         else:
             for _ in range(generations):
                 self.evolve()
-                pop_history.append(self.population.copy())
+                pop_history.append(self.population.individuals.copy())
         if history:
-            if self.encoding == "binary":
-                pop_history = [[self.decode(ind) for ind in gen] for gen in pop_history]
+            pop_history = [[self.population.encoding.decode(ind) for ind in gen] for gen in pop_history]
             return pop_history
 
     def fitness_plot(self) -> None:
--- metazoo/src/metazoo/bio/evolutionary/utils.py
+++ metazoo/src/metazoo/bio/evolutionary/utils.py
@@ -1,34 +1,92 @@
+from abc import ABC, abstractmethod
 import numpy as np
+from typing import Sequence, Tuple
 
+class Encoding(ABC):
+    @abstractmethod
+    def encode(self, population_size: int) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def decode(self, individual: np.ndarray) -> np.ndarray:
+        pass
+
+class Binary(Encoding):
+    def __init__(self, precision: int = 3, bounds: Sequence[Tuple[float, float]] = None):
+        self.precision = precision
+        self.bounds: Sequence[Tuple[float, float]] = bounds
+        if bounds is None:
+            raise ValueError("Bounds must be provided for binary encoding.")
+        self.dim = len(bounds)  # Number of variables
+
+        self.epsilon = 10 ** (
+            -precision
+        )  # ε = Obtaianable accuracy for binary encoding
+
+        # Bits per variable for binary encoding
+        # This is calculated based on the precision and the range of each variable
+        # Using the formula: n = ceil(log2((XU - XL) / ε))
+        # where XU and XL are the upper and lower bounds of the variable, and ε is the desired precision
+        # We can understand ε as the smallest difference we want to be able to represent between two values of the variable
+        self.bits_per_var = max(
+            int(np.ceil(np.log2((xu - xl) / self.epsilon))) for xl, xu in self.bounds
+        )
+        # We use max to ensure we have enough bits for the most constrained variable
+
+        self.genome_length = self.bits_per_var * self.dim
+
+    def decode(self, individual: np.ndarray) -> np.ndarray:
+        """
+        Decodes a binary individual into its real-valued representation.
+        """
+        bits_per_var = self.bits_per_var
+        decoded = []
+        for i, (a, b) in enumerate(self.bounds):
+            bits = individual[i * bits_per_var : (i + 1) * bits_per_var]
+            value = int("".join(str(int(bit)) for bit in bits), 2)
+            max_value = 2**bits_per_var - 1
+            real_value = a + (b - a) * value / max_value
+            decoded.append(real_value)
+        return np.array(decoded)
+
+    def encode(self, population_size: int) -> np.ndarray:
+        return np.random.randint(0, 2, size=(population_size, self.genome_length))
+
+class Real(Encoding):
+    def __init__(self, bounds: Sequence[Tuple[float, float]] = None):
+        self.bounds: Sequence[Tuple[float, float]] = bounds
+        if bounds is None:
+            raise ValueError("Bounds must be provided for real encoding.")
+        self.dim = len(bounds)  # Number of variables
+        self.genome_length = self.dim
+
+    def decode(self, individual: np.ndarray) -> np.ndarray:
+        return individual  # For real encoding, the individual is already in real values
+
+    def encode(self, population_size: int):
+        arr = np.empty((population_size, self.genome_length))
+        for j, (low, high) in enumerate(self.bounds):
+            arr[:, j] = np.random.uniform(low, high, size=population_size)
+        return arr
+
+class Permutation(Encoding):
+    def __init__(self, permutation_size: int):
+        self.permutation_size = permutation_size
+        self.genome_length = permutation_size
+
+    def decode(self, individual: np.ndarray) -> np.ndarray:
+        return individual  # For permutation encoding, the individual is already in permutation form
+
+    def encode(self, population_size: int) -> np.ndarray:
+        return np.array([np.random.permutation(self.genome_length) for _ in range(population_size)])
+
+class encoding:
+    Binary = Binary
+    Real = Real
+    Permutation = Permutation
 
 class Population:
-    def __init__(self, population_size, genome_length, encoding="real", bounds=(-1, 1)):
-        self.population_size = population_size
-        self.genome_length = genome_length
+    def __init__(self, population_size: int, encoding: Encoding):
+        self.size = population_size
         self.encoding = encoding
-        self.bounds = bounds
-
-    def initialize(self):
-        if self.encoding == "binary":
-            return np.random.randint(
-                0, 2, size=(self.population_size, self.genome_length)
-            )
-        elif self.encoding == "boolean":
-            return np.random.choice(
-                [True, False], size=(self.population_size, self.genome_length)
-            )
-        elif self.encoding == "real":
-            if isinstance(self.bounds, (list, tuple)) and isinstance(
-                self.bounds[0], (list, tuple)
-            ):
-                arr = np.empty((self.population_size, self.genome_length))
-                for j, (low, high) in enumerate(self.bounds):
-                    arr[:, j] = np.random.uniform(low, high, size=self.population_size)
-                return arr
-            else:
-                low, high = self.bounds
-                return np.random.uniform(
-                    low, high, size=(self.population_size, self.genome_length)
-                )
-        else:
-            raise ValueError("Encoding must be 'binary', 'boolean' o 'real'")
+        self.individuals = encoding.encode(population_size)
